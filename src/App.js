1cc50894668c8b3fb551ae68d07a3f63a417885a
RAHUL YADAV <rahulyadav@RAHULs-MacBook-Air.local>
2023-12-15 22:15:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FcSportsMode } from "react-icons/fc";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 function App() {
+  const [isOpen, setOpen] = useState(false);
+
+  const handleExpandability = () => {
+    setOpen(!isOpen);
+  };
+
+  const transition = { type: "spring", duration: 0.8 };
+  const spring = {
+    type: "spring",
+    stiffness: 700,
+    damping: 30,
+  };
+  const fadeAnimation = {
+    initial: {
+      opacity: 0,
+      transition: { ...transition, delay: 0 },
+    },
+    animate: {
+      opacity: 1,
+
+      transition: { ...transition, delay: 0.2 },
+    },
+    exit: {
+      opacity: 0,
+      transition: { ...transition, delay: 0 },
+    },
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="wrapperContainer ">
+        <motion.div
+          className="expandableContainer "
+          onClick={handleExpandability}
+          data-isOpen={isOpen}
+          layout
+          transition={spring}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="buttonContainer">
+            <div className="title flex gap-2">
+              <motion.p layout="position">Framer-motion</motion.p>
+              <span className="text-2xl">
+                <FcSportsMode />
+              </span>
+            </div>
+          </div>
+          <AnimatePresence>
+            {isOpen && (
+              <motion.div
+                layout="position"
+                className="contentContainer py-2"
+                {...fadeAnimation}
+              >
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Veniam nesciunt iure suscipit a quae consequatur mollitia
+                  harum maiores consectetur, quis velit dolorum ipsam laboriosam
+                  deserunt nostrum tempore minus, natus asperiores.
+                </p>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </motion.div>
+      </div>
+    </>
   );
 }
 
